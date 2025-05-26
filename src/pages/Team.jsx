22da3a0d3ea471f343827a54dff48fb4d70a2a6b
rthrx44 <bkbrx44@gmail.com>
2025-05-27 02:46:39 +0800

    done implement team section
    done implementing team section

---
+++ src/pages/Team.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import team from "../assets/team.png";
+import { Button } from "../components/Button";
+
+const Team = () => {
+  return (
+    <section className="w-full mt-24 flex flex-col md:flex-row justify-center items-center">
+      <div className="w-full order-2 flex justify-center sm:pr-[5.625rem] xl:pr-[9.375rem]">
+        <img src={team} alt="People Planning" />
+      </div>
+      <div className="w-full mb-10 md:mb-0 sm:pl-[5.625rem] xl:pl-[9.375rem]">
+        <div className="flex flex-col gap-10 md:gap-5 lg:gap-10 lg:w-[36.25rem] mx-auto">
+          <h1 className="font-alice text-center md:text-left">
+            Be Part Of Our Team
+          </h1>
+          <p className="font-corbel text-center px-4 sm:px-[5.625rem] md:px-0 md:text-left">
+            Explore opportunities to take your career to the next level. Join
+            our team of talented individuals and let us support your curiosity
+            and creative talent.
+          </p>
+          <div className="flex justify-center md:justify-start">
+            <Button displayText="LEARN MORE" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Team;
--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -25,20 +25,28 @@ const Testimonial = () => {
           alt="Square Big Screen Background"
         />
         <div className="w-full absolute top-0 h-full flex flex-col justify-center gap-10 md:gap-5 xl:gap-10">
-          <div className="flex flex-col items-center text-center font-alice gap-10 md:gap-5 xl:gap-10">
-            <img className="w-16" src={right} alt="Right Quote" />
-            <h3 className="px-4 sm:px-[5.625rem] xl:px-[9.375rem]">
-              It is very satisfying when a decision we make turns out to have
-              been the best one.
-            </h3>
-            <h3>Jane Doe</h3>
+          <div className="flex flex-row items-center justify-center px-[1rem] sm:px-[5.625rem] xl:px-[9.375rem]">
+            <div className="button-prev-slide hidden md:flex">
+              <MoveLeft className="hover:text-[#F7941D] transition-all w-8 h-8 md:w-[2.625rem] md:h-9" />
+            </div>
+            <div className="w-full flex flex-col items-center text-center font-alice gap-10 md:gap-5 xl:gap-10">
+              <img className="w-16" src={right} alt="Right Quote" />
+              <h3 className="px-4 sm:px-[5.625rem] xl:px-[9.375rem]">
+                It is very satisfying when a decision we make turns out to have
+                been the best one.
+              </h3>
+              <h3>Jane Doe</h3>
+            </div>
+            <div className="button-next-slide hidden md:flex">
+              <MoveRight className="hover:text-[#F7941D] transition-all w-8 h-8 md:w-[2.625rem] md:h-9" />
+            </div>
           </div>
-          <div className="flex justify-center gap-12 mt-8">
+          <div className="flex justify-center gap-12 mt-8 md:hidden">
             <div className="button-prev-slide">
-              <MoveLeft className="hover:text-[#F7941D] transition-all" />
+              <MoveLeft className="hover:text-[#F7941D] transition-all w-8 h-8 md:w-[2.625rem] md:h-9" />
             </div>
             <div className="button-next-slide">
-              <MoveRight className="hover:text-[#F7941D] transition-all" />
+              <MoveRight className="hover:text-[#F7941D] transition-all w-8 h-8 md:w-[2.625rem] md:h-9" />
             </div>
           </div>
         </div>
